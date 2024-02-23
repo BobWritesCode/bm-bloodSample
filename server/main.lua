922aaae325f65e188fcfb3bf19c2a383ecf82462
BobWritesCode <warwick_hart@hotmail.com>
2024-02-23 11:44:56 +0000

    add tprint()

--- server/main.lua
+++ server/main.lua
@@ -93,3 +93,18 @@ QBCore.Functions.CreateCallback('bm-bloodEvidence:server:createNewReport',
         cb(404)
       end)
   end)
+
+function tprint(tbl, indent)
+  if not indent then indent = 0 end
+  for k, v in pairs(tbl) do
+    local formatting = string.rep("  ", indent) .. k .. ": "
+    if type(v) == "table" then
+      print(formatting)
+      tprint(v, indent + 1)
+    elseif type(v) == 'boolean' then
+      print(formatting .. tostring(v))
+    else
+      print(formatting .. v)
+    end
+  end
+end
