c09e93aeacb4bbac9dc3bf808f6e3647e899bf36
BobWritesCode <warwick_hart@hotmail.com>
2024-03-14 15:26:37 +0000

    move functions to utils

--- server/main.lua
+++ server/main.lua
@@ -155,34 +155,3 @@ QBCore.Functions.CreateCallback('bm-bloodEvidence:server:getReport',
         cb(404, {})
       end)
   end)
-
-function GetDateTime()
-  local month_abbr = {
-    "Jan", "Feb", "Mar", "Apr", "May", "Jun",
-    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
-  }
-  local current_time = os.date("*t")
-  local year = current_time.year
-  local month = current_time.month
-  local day = current_time.day
-  local hour = current_time.hour
-  local min = current_time.min
-  local sec = current_time.sec
-  local dateTimeString = string.format("%02d-%s-%02d %02d:%02d:%02d", day, month_abbr[month], year, hour, min, sec)
-  return dateTimeString
-end
-
-function Tprint(tbl, indent)
-  if not indent then indent = 0 end
-  for k, v in pairs(tbl) do
-    local formatting = string.rep("  ", indent) .. k .. ": "
-    if type(v) == "table" then
-      print(formatting)
-      Tprint(v, indent + 1)
-    elseif type(v) == 'boolean' then
-      print(formatting .. tostring(v))
-    else
-      print(formatting .. v)
-    end
-  end
-end
